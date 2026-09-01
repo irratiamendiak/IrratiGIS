651e9a1c501ba14314b22af0e90e09f54e0c7221
irratiamendiak <kulixka.mendiak@gmail.com>
2026-09-01 22:21:37 +0200

    Bust fire popup cache after burn layer fix

--- auth.js
+++ auth.js
@@ -37,7 +37,7 @@
     if (existing) return window.IrratiGISFirePopupReady || Promise.resolve(window.IrratiGISFirePopup);
     const script = document.createElement("script");
     script.id = "irratiFirePopupScript";
-    script.src = "fire-popup.js?v=20260901-3";
+    script.src = "fire-popup.js?v=20260901-4";
     script.defer = true;
     window.IrratiGISFirePopupReady = new Promise(resolve => {
       script.onload = () => resolve(window.IrratiGISFirePopup);
