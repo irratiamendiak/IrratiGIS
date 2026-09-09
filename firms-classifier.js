7091b2a74ac5feaedc07050d7ac1239e5f7ceb42
irratiamendiak <kulixka.mendiak@gmail.com>
2026-09-09 07:29:56 +0200

    Ajustar clasificación FIRMS al contexto industrial inmediato

--- firms-classifier.js
+++ firms-classifier.js
@@ -1,6 +1,6 @@
 (()=>{
 "use strict";
-const VERSION="20260909-01";
+const VERSION="20260907-11";
 const INDUSTRIAL_TAGS=["industrial","quarry","brownfield","works","kiln","plant","chimney","storage_tank","silo","power","generator","substation","landfill"];
 const FOREST_TAGS=["forest","wood","scrub","heath","fell"];
 const VEGETATION_TAGS=["forest","wood","scrub","heath","fell","farmland","meadow","orchard","vineyard","grassland","allotments"];
